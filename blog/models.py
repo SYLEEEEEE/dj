e85a0a1a2e30e3a3d5612fa5fa56392abde48ab1
SYLEEEEEE <lsy5345@gmail.com>
2022-08-05 08:54:28 +0900

    haha

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
+from operator import mod
 from django.db import models
+from matplotlib.pyplot import title
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'no.{self.pk} {self.title}'  # 처음에 순서대로 부여된 pk가 적용됨
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+class PostList(ListView):
+    model = Post
+    ordering= '-pk'
+    # template_name = 'blog/index.html'
+
+class PostDetail(DetailView):
+    model = Post
+
+# def single_post_page(request, pk):
+#     post = Post.object.get(pk=pk)
+
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post':post,
+#         }
+    # )
+# def index(request):
+#     posts = Post.objects.all()
+
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts':posts,
+#         }
+#     )
+
+def post_list(request):
+    posts = Post.objects.all()
