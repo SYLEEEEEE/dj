23914194700fbef94be5eeac91c4fde9fe0f735b
SYLEEEEEE <lsy53475@gmail.com>
2022-08-08 21:56:56 +0900

    hh

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,27 @@
 from operator import mod
 from django.db import models
 from matplotlib.pyplot import title
+import os
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    head_image = models.ImageField(upload_to='blog/image/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return f'no.{self.pk} {self.title}'  # 처음에 순서대로 부여된 pk가 적용됨
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
