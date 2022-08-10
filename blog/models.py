bee63ff0479fba51ebeeac1d1f4e9f228476bdf6
SYLEEEEEE <lsy53475@gmail.com>
2022-08-10 09:04:00 +0900

    hh

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,23 @@
 from operator import mod
+from unicodedata import category
 from django.db import models
 from matplotlib.pyplot import title
+from django.contrib.auth.models import User
 import os
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+    
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -14,8 +30,12 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='blog/image/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
     def __str__(self):
-        return f'no.{self.pk} {self.title}'  # 처음에 순서대로 부여된 pk가 적용됨
+        return f'no.{self.pk} {self.title} :: {self.author}'  # 처음에 순서대로 부여된 pk가 적용됨
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 from django.views.generic import ListView, DetailView
 
 class PostList(ListView):
@@ -7,8 +7,39 @@ class PostList(ListView):
     ordering= '-pk'
     # template_name = 'blog/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        }
+    )
+
 class PostDetail(DetailView):
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
 # def single_post_page(request, pk):
 #     post = Post.object.get(pk=pk)
