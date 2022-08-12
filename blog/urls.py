b1f2f71576010c98954d873d2c4a82189d084898
SYLEEEEEE <lsy53475@gmail.com>
2022-08-12 09:23:52 +0900

    hh

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
+    path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('', views.PostList.as_view()),#blog/ 뒤에 아무것도 없으면 포스트리스트로
