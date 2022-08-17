9ba6b162eddf2474fbb5dbfff543585e175805c7
SYLEEEEEE <lsy53475@gmail.com>
2022-08-17 12:18:06 +0900

    search

--- blog/views.py
+++ blog/views.py
@@ -7,11 +7,13 @@ from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 from .forms import CommentForm
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 #CBV
 class PostList(ListView):
     model = Post
-    ordering= '-pk'
+    ordering = '-pk'
+    paginate_by = 5
     # template_name = 'blog/index.html'
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
@@ -173,3 +175,20 @@ def delete_comment(request, pk):
         return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied # 요청한 유저가 코멘트 작성자가 아니면 허가거부
+
+class PostSearch(PostList):
+    paginate_by = None
+    
+    def get_queryset(self):
+        q = self.kwargs['q']
+        post_list = Post.objects.filter(
+            Q(title__contains=q) | Q(tags__name__contains=q)
+        ).distinct()
+        return post_list
+
+    def get_context_data(self, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+
+        return context
